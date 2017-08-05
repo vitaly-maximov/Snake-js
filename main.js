4b11d0b93fd44f2674f0293ba4a31ca2764403c8
Vitalii Maksimov <g.vitaly.maximov@gmail.com>
2017-08-05 20:49:45 +0300

    1. Game class was added. It will contain game logic. 2. Visual class was added. It draws everything. 3. Unit was added. Snake should it them.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//"use strict";
+
 let canvasElement = document.getElementById("canvas");
 let canvasContext = canvasElement.getContext("2d");
 
@@ -15,35 +17,40 @@ class Direction {
 /*---------------------------------------------------------------------------*/
 class Snake {
 
-	constructor(head)
+	constructor(head, direction)
 	{
 		// TODO: assert
 
 		this._body = [head];
+        this._direction = direction;
 	}
 
-	// TODO:
-	get body()
-	{
-		return this._body;
-	}
+    get head() { return this._body[this._body.length - 1]; }
+    
+	get body() { return this._body; }
+
+    get direction() { return this._direction; }
+
+    set direction(value) {
+        // TODO: assert
+
+        this._direction = value;
+    }
 
 	// TODO:
 	increase()
 	{
-		let head = this._body[this._body.length - 1];
-
-		this._body[this._body.length] = {
-			x: head.x,
-			y: head.y + 1
-		}
+        this._body.push({
+            x: this.head.x,
+            y: this.head.y - 1
+        });
 	}	
 
-	moveTo(direction)
+	move()
 	{		
 		let head = this._body[this._body.length - 1];
 		this._body.shift();
-		switch (direction)
+		switch (this._direction)
 		{
 			case Direction.up: this._body.push({ x: head.x, y: head.y - 1 }); break;
 			case Direction.down: this._body.push({ x: head.x, y: head.y + 1 }); break;
@@ -51,11 +58,10 @@ class Snake {
 			case Direction.right: this._body.push({ x: head.x + 1, y: head.y }); break;
 		}
 	}
-
-
 }
 /*---------------------------------------------------------------------------*/
 class Field {
+
 	constructor(width, height)
 	{
 		// TODO: assert
@@ -79,72 +85,166 @@ class Field {
 
 	get height() { return this._height; }
 
-	isWall(row, column) {
+	isWall(row, column) 
+    {
 		return this._cells[row][column];
 	}
 }
+/*---------------------------------------------------------------------------*/
+class Unit {
+    constructor(x, y)
+    {
+        this._x = x;
+        this._y = y;
+    }
 
-let drawSnake = (snake) => {
-	const offset = 10;
+    get x() { return this._x; }
 
-	const blockSize = 30;
-	const blockMargin = 3;
-	const borderWidth = 1;
-
-	for (let part of snake.body)
-	{
-		canvasContext.fillStyle = "#00FF00";
-
-		canvasContext.fillRect(
-					offset + part.x * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
-					offset + part.y * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
-					blockSize,
-					blockSize);
-	}
+    get y() { return this._y; }
 }
+/*---------------------------------------------------------------------------*/
+class Game {
+    constructor() 
+    {
+        this._field = new Field(13, 17);
+        this._snake = new Snake({ x: 7, y: 9 }, Direction.up);
+        this._units = [];
 
-let drawField = (field) => {	
-	const offset = 10;
+        this._time = 0;
 
-	const blockSize = 30;
-	const blockMargin = 3;
-	const borderWidth = 1;
+        // TODO:
+        this._snake.increase();
+        this._snake.increase();
+        this._snake.increase();   
+    }
 
-	width = field.width * (blockSize + 2 * blockMargin) + 2 * borderWidth;
-	height = field.height * (blockSize + 2 * blockMargin) + 2 * borderWidth;
+    get field() { return this._field; }
 
-	canvasContext.fillStyle = "#000000";
-	canvasContext.rect(offset, offset, width, height);	
+    get snake() { return this._snake; }
 
-	for (let row = 1; row < field.height; ++row)
-	{
-		canvasContext.moveTo(offset, offset + row * (blockSize + 2 * blockMargin) + borderWidth);
-		canvasContext.lineTo(offset + width, offset + row * (blockSize + 2 * blockMargin) + borderWidth);
-	}
+    get units() { return this._units; }
 
-	for (let column = 1; column < field.width; ++column)
-	{
-		canvasContext.moveTo(offset + column * (blockSize + 2 * blockMargin) + borderWidth, offset);
-		canvasContext.lineTo(offset + column * (blockSize + 2 * blockMargin) + borderWidth, offset + height)
-	}
+    process()
+    {
+        this._time += 1;
 
-	for (let row = 0; row < field.height; ++row)
-	{
-		for (let column = 0; column < field.width; ++column)
-		{
-			if (field.isWall(row, column))
-			{
-				canvasContext.fillRect(
-					offset + column * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
-					offset + row * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
-					blockSize,
-					blockSize);
-			}	
-		}
-	}
+        if (this._time % 10 === 0)
+        {
+            let random = a => Math.floor(a * Math.random());
 
-	canvasContext.stroke();	
+            let x = random(this._field.width);
+            let y = random(this._field.height);
+
+            this._units.push(new Unit(x, y));
+        }
+
+        this._snake.move();
+    }
 }
+/*---------------------------------------------------------------------------*/
+class Visual {
+
+    constructor(game) 
+    {
+        this._game = game;
+    }
+
+    draw() 
+    {
+        canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+        this.drawField(this._game.field);
+        this.drawSnake(this._game.snake);
+        this.drawUnits(this._game.units);
+    }
+
+    drawSnake(snake) 
+    {
+        const offset = 10;
+
+        const blockSize = 30;
+        const blockMargin = 3;
+        const borderWidth = 1;
+
+        for (let part of snake.body)
+        {
+            canvasContext.fillStyle = "#00FF00";
+
+            canvasContext.fillRect(
+                        offset + part.x * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
+                        offset + part.y * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
+                        blockSize,
+                        blockSize);
+        }
+    }
+
+    drawField(field) 
+    {
+        const offset = 10;
+
+        const blockSize = 30;
+        const blockMargin = 3;
+        const borderWidth = 1;
+
+        let width = field.width * (blockSize + 2 * blockMargin) + 2 * borderWidth;
+        let height = field.height * (blockSize + 2 * blockMargin) + 2 * borderWidth;
+
+        canvasContext.fillStyle = "#000000";
+        canvasContext.rect(offset, offset, width, height);  
+
+        for (let row = 1; row < field.height; ++row)
+        {
+            canvasContext.moveTo(offset, offset + row * (blockSize + 2 * blockMargin) + borderWidth);
+            canvasContext.lineTo(offset + width, offset + row * (blockSize + 2 * blockMargin) + borderWidth);
+        }
+
+        for (let column = 1; column < field.width; ++column)
+        {
+            canvasContext.moveTo(offset + column * (blockSize + 2 * blockMargin) + borderWidth, offset);
+            canvasContext.lineTo(offset + column * (blockSize + 2 * blockMargin) + borderWidth, offset + height)
+        }
+
+        for (let row = 0; row < field.height; ++row)
+        {
+            for (let column = 0; column < field.width; ++column)
+            {
+                if (field.isWall(row, column))
+                {
+                    canvasContext.fillRect(
+                        offset + column * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
+                        offset + row * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
+                        blockSize,
+                        blockSize);
+                }   
+            }
+        }
+
+        canvasContext.stroke(); 
+    }
+
+    drawUnits(units)
+    {
+        const offset = 10;
+
+        const blockSize = 30;
+        const blockMargin = 3;
+        const borderWidth = 1;
+
+        canvasContext.fillStyle = "#008800";
+
+        for (let unit of units)
+        {
+            canvasContext.fillRect(
+                            offset + unit.x * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
+                            offset + unit.y * (blockSize + 2 * blockMargin) + blockMargin + borderWidth,
+                            blockSize,
+                            blockSize);
+        }
+    }
+}
+/*---------------------------------------------------------------------------*/
+let game = new Game();
+let visual = new Visual(game);
 
 let doLayout = () => {	
 	const offset = 20;
@@ -153,41 +253,27 @@ let doLayout = () => {
 	canvasElement.height = window.innerHeight - offset;
 }
 
-let snake = new Snake({ x: 5, y: 5 }, Direction.up);
-	snake.increase();
-	snake.increase();
-	snake.increase();	
-
-let draw = () => {
-	canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
-	drawField(new Field(10, 15));
-	
-	drawSnake(snake);
-}
-
 window.onload = () => {	
 	doLayout();
-	draw();
+    visual.draw();
 }
 
 window.onresize = () => {
 	doLayout();
-	draw();
+	visual.draw();
 }
-
-var globalDirection = Direction.up;
 
 window.addEventListener('keydown', args => {
 	switch (args.keyCode)
 	{
-		case 37: globalDirection = Direction.left; break;
-		case 38: globalDirection = Direction.up; break;
-		case 39: globalDirection = Direction.right; break;
-		case 40: globalDirection = Direction.down; break;
+		case 37: game.snake.direction = Direction.left; break;
+		case 38: game.snake.direction = Direction.up; break;
+		case 39: game.snake.direction = Direction.right; break;
+		case 40: game.snake.direction = Direction.down; break;
 	}
 });
 
 setInterval(() => { 
-	snake.moveTo(globalDirection);
-	draw(); 
+    game.process();	
+	visual.draw();
 }, 1000)
